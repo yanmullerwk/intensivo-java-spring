cab390e6e2595251e8061d3559e363ccce49b581
yanmullerwk <yan.muller.contact@gmail.com>
2025-01-22 18:19:54 -0300

    create: entity game

---
+++ my-game-hank/src/main/java/br/com/yanmuller/MyGameHankApplication.java
@@ -0,0 +1,13 @@
+package br.com.yanmuller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameHankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameHankApplication.class, args);
+	}
+
+}
